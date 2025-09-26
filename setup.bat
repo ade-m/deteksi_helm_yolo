2d1f237e7869e0718953d9913851ad96942123b1
Ade Maulana <66313509+ade-m@users.noreply.github.com>
2025-09-26 09:02:48 +0700

    Add files via upload

--- setup.bat
+++ setup.bat
@@ -8,11 +8,11 @@ python -m venv yolov5_venv
 echo Virtual environment 'yolov5_venv' dibuat.
 
 REM 2. Aktifkan venv
-call yolov5_venv\Scripts\activate
+call yolov5_venv\Scripts\activate.bat
 echo Virtual environment aktif.
 
 REM 3. Upgrade pip
-python -m pip install --upgrade pip
+pip install --upgrade pip
 
 REM 4. Install dependencies
 pip install -r requirements.txt
@@ -20,7 +20,6 @@ echo Semua dependencies terinstall.
 
 REM 5. Instruksi setelah setup
 echo Setup selesai. Untuk menjalankan training:
-echo 1. Aktifkan venv: call yolov5_venv\Scripts\activate
+echo 1. Aktifkan venv: call yolov5_venv\Scripts\activate.bat
 echo 2. Jalankan script training: python train_yolo.py
-
 pause
